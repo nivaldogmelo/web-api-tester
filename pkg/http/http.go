682d02b5642fb3ca50450a509ad16160ba5bc550
Nivaldo Melo <nivaldogmelo@gmail.com>
2021-01-03 17:07:17 -0300

    Remove superfluous http header write

--- pkg/http/http.go
+++ pkg/http/http.go
@@ -17,7 +17,6 @@ func respondWithError(w http.ResponseWriter, code int, msg string) {
 func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
 	response, _ := json.Marshal(payload)
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
 	w.Write(response)
 }
 
