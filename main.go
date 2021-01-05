6d5d9358bccddbbfe39f242ba5c39a203c2503ec
Nivaldo Melo <nivaldogmelo@gmail.com>
2021-01-05 00:58:27 -0300

    Read port from config file

--- main.go
+++ main.go
@@ -1,19 +1,39 @@
 package main
 
 import (
+	"errors"
 	"log"
 
 	web "github.com/nivaldogmelo/web-api-tester/cmd"
+	c "github.com/nivaldogmelo/web-api-tester/config"
+	error_handler "github.com/nivaldogmelo/web-api-tester/pkg/error"
 	sqlite "github.com/nivaldogmelo/web-api-tester/pkg/sqlite"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	log.Println("Starting to serve at port 3000...")
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
+	viper.SetConfigType("yaml")
 
-	err := sqlite.InitDB()
+	var config c.Config
+	if err := viper.ReadInConfig(); err != nil {
+		error_handler.Print(errors.New("Error reading config file, using default name"))
+		log.Fatal(err)
+	}
+
+	err := viper.Unmarshal(&config)
+	if err != nil {
+		error_handler.Print(errors.New("Error parsing config file using default name"))
+		log.Fatal(err)
+	}
+
+	err = sqlite.InitDB()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	web.StartServer(":3000")
+	log.Println("Starting to serve at port " + config.Server.Port + "...")
+	web.StartServer(":" + config.Server.Port)
 }
